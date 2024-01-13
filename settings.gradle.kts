54becc768c475f299396eba1091e2dcc4714074f
tosoba <therealmerengue@gmail.com>
2024-01-13 14:27:47 +0100

    Override project using kmp wizard: https://kmp.jetbrains.com/

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,22 +1,21 @@
+rootProject.name = "alarmist"
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 pluginManagement {
-  repositories {
-    google()
-    gradlePluginPortal()
-    mavenCentral()
-  }
+    repositories {
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
 }
 
 dependencyResolutionManagement {
-  repositories {
-    google()
-    mavenCentral()
-  }
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
 }
 
-rootProject.name = "Alarmist"
-
-include(":androidApp")
-
-include(":shared")
+include(":composeApp")
--- shared/src/commonMain/kotlin/com/trm/alarmist/Platform.kt
+++
@@ -1,7 +0,0 @@
-package com.trm.alarmist
-
-interface Platform {
-  val name: String
-}
-
-expect fun getPlatform(): Platform
