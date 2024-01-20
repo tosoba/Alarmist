e28d558b88ef67db8271a3f0dc47ceb38dd089c2
tosoba <therealmerengue@gmail.com>
2024-01-20 18:06:31 +0100

    add root nav methods

--- composeApp/src/commonMain/kotlin/com/trm/alarmist/feature/root/RootComponent.kt
+++ composeApp/src/commonMain/kotlin/com/trm/alarmist/feature/root/RootComponent.kt
@@ -4,6 +4,8 @@ import com.arkivanov.decompose.ComponentContext
 import com.arkivanov.decompose.router.stack.ChildStack
 import com.arkivanov.decompose.router.stack.StackNavigation
 import com.arkivanov.decompose.router.stack.childStack
+import com.arkivanov.decompose.router.stack.pop
+import com.arkivanov.decompose.router.stack.push
 import com.arkivanov.decompose.value.Value
 import com.trm.alarmist.feature.alarm.AlarmComponent
 import com.trm.alarmist.feature.alarm.DefaultAlarmComponent
@@ -14,10 +16,17 @@ import com.trm.alarmist.feature.group.GroupComponent
 import kotlinx.serialization.Serializable
 
 interface RootComponent {
-
   val childStack: Value<ChildStack<*, Child>>
 
-  fun onBackClicked()
+  fun onAddAlarmClick()
+
+  fun onEditAlarmClick()
+
+  fun onAddGroupClick()
+
+  fun onEditGroupClick()
+
+  fun onBackClick()
 
   sealed interface Child {
     class Alarms(val component: AlarmsComponent) : Child
@@ -31,38 +40,59 @@ interface RootComponent {
 class DefaultRootComponent(
     componentContext: ComponentContext,
 ) : RootComponent, ComponentContext by componentContext {
-  private val navigation = StackNavigation<Config>()
+  private val navigation = StackNavigation<ChildConfig>()
 
   override val childStack: Value<ChildStack<*, RootComponent.Child>> =
       childStack(
           source = navigation,
-          serializer = Config.serializer(),
-          initialConfiguration = Config.Alarms,
+          serializer = ChildConfig.serializer(),
+          initialConfiguration = ChildConfig.Alarms,
           handleBackButton = true,
           childFactory = ::createChild,
       )
 
-  private fun createChild(config: Config, componentContext: ComponentContext): RootComponent.Child =
+  private fun createChild(
+      config: ChildConfig,
+      componentContext: ComponentContext
+  ): RootComponent.Child =
       when (config) {
-        is Config.Alarm -> {
+        is ChildConfig.Alarm -> {
           RootComponent.Child.Alarm(DefaultAlarmComponent(config.mode, componentContext))
         }
-        Config.Alarms -> {
+        ChildConfig.Alarms -> {
           RootComponent.Child.Alarms(DefaultAlarmsComponent(componentContext))
         }
-        is Config.Group -> {
+        is ChildConfig.Group -> {
           RootComponent.Child.Group(DefaultGroupComponent(config.mode, componentContext))
         }
       }
 
-  override fun onBackClicked() {}
+  override fun onBackClick() {
+    navigation.pop()
+  }
+
+  override fun onAddAlarmClick() {
+    navigation.push(ChildConfig.Alarm(AlarmComponent.Mode.Add))
+  }
+
+  override fun onEditAlarmClick() {
+    navigation.push(ChildConfig.Alarm(AlarmComponent.Mode.Edit))
+  }
+
+  override fun onAddGroupClick() {
+    navigation.push(ChildConfig.Group(GroupComponent.Mode.Add))
+  }
+
+  override fun onEditGroupClick() {
+    navigation.push(ChildConfig.Group(GroupComponent.Mode.Edit))
+  }
 
   @Serializable
-  private sealed interface Config {
-    @Serializable data object Alarms : Config
+  private sealed interface ChildConfig {
+    @Serializable data object Alarms : ChildConfig
 
-    @Serializable data class Alarm(val mode: AlarmComponent.Mode) : Config
+    @Serializable data class Alarm(val mode: AlarmComponent.Mode) : ChildConfig
 
-    @Serializable data class Group(val mode: GroupComponent.Mode) : Config
+    @Serializable data class Group(val mode: GroupComponent.Mode) : ChildConfig
   }
 }
