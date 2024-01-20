46742b76ebd083d138cef605cb8b304e5bbd64e7
tosoba <therealmerengue@gmail.com>
2024-01-20 13:25:36 +0100

    packages + remove sample files

--- composeApp/src/commonMain/kotlin/com/trm/alarmist/App.kt
+++ composeApp/src/commonMain/kotlin/com/trm/alarmist/App.kt
@@ -1,3 +1,5 @@
+package com.trm.alarmist
+
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
@@ -20,13 +22,11 @@ import org.jetbrains.compose.resources.painterResource
 fun App() {
   MaterialTheme {
     var showContent by remember { mutableStateOf(false) }
-    val greeting = remember { Greeting().greet() }
     Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
       Button(onClick = { showContent = !showContent }) { Text("Click me!") }
       AnimatedVisibility(showContent) {
         Column(Modifier.fillMaxWidth(), horizontalAlignment = Alignment.CenterHorizontally) {
           Image(painterResource("compose-multiplatform.xml"), null)
-          Text("Compose: $greeting")
         }
       }
     }
--- composeApp/src/iosMain/kotlin/com/trm/alarmist/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/trm/alarmist/MainViewController.kt
@@ -1,3 +1,6 @@
+package com.trm.alarmist
+
 import androidx.compose.ui.window.ComposeUIViewController
+import com.trm.alarmist.App
 
 fun MainViewController() = ComposeUIViewController { App() }
