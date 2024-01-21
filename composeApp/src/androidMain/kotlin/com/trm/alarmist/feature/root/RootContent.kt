c771d9a7f96ca04c0f35a3cb0a8a42ff09e09cd8
tosoba <therealmerengue@gmail.com>
2024-01-21 13:16:44 +0100

    add root toolbar nav icon

--- composeApp/src/androidMain/kotlin/com/trm/alarmist/feature/root/RootContent.kt
+++ composeApp/src/androidMain/kotlin/com/trm/alarmist/feature/root/RootContent.kt
@@ -3,8 +3,13 @@ package com.trm.alarmist.feature.root
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -29,19 +34,50 @@ fun RootContent(modifier: Modifier = Modifier, component: RootComponent) {
         Text(
           text =
             when (val active = childStack.active.instance) {
-              is RootComponent.Child.Alarm ->
+              is RootComponent.Child.Alarms -> {
+                "Alarms"
+              }
+              is RootComponent.Child.Alarm -> {
                 when (active.component.mode) {
                   AlarmComponent.Mode.Add -> "New alarm"
                   AlarmComponent.Mode.Edit -> "Edit alarm"
                 }
-              is RootComponent.Child.Alarms -> "Alarms"
-              is RootComponent.Child.Group ->
+              }
+              is RootComponent.Child.Group -> {
                 when (active.component.mode) {
                   GroupComponent.Mode.Add -> "New group"
                   GroupComponent.Mode.Edit -> "Edit group"
                 }
+              }
             }
         )
+      },
+      navigationIcon = {
+        IconButton(
+          onClick = {
+            when (childStack.active.instance) {
+              is RootComponent.Child.Alarms -> {}
+              else -> {
+                component.onBackClick()
+              }
+            }
+          }
+        ) {
+          when (childStack.active.instance) {
+            is RootComponent.Child.Alarms -> {
+              Icon(
+                imageVector = Icons.Default.Menu,
+                contentDescription = "Menu",
+              )
+            }
+            else -> {
+              Icon(
+                imageVector = Icons.Default.ArrowBack,
+                contentDescription = "Back",
+              )
+            }
+          }
+        }
       }
     )
 
