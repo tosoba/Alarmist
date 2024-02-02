bc39a878c944d69d5ad21708a8e19c57fdcf0bcb
tosoba <therealmerengue@gmail.com>
2024-02-02 17:47:55 +0100

    move decompose extensions dependency to android

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -27,6 +27,7 @@ kotlin {
       implementation(libs.compose.ui.tooling.preview)
 
       implementation(libs.decompose.extensions.android)
+      implementation(libs.decompose.extensions.compose)
 
       implementation(libs.essenty.statekeeper.android)
 
@@ -44,7 +45,6 @@ kotlin {
       @OptIn(ExperimentalComposeLibrary::class) implementation(compose.components.resources)
 
       implementation(libs.decompose)
-      implementation(libs.decompose.extensions.compose)
       implementation(libs.essenty.lifecycle)
       implementation(libs.essenty.statekeeper)
 
