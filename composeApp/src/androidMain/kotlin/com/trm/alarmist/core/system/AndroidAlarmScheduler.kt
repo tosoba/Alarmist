3cc91259a3825b864049c87c55577c98612434f5
Mere Ngue <therealmerengue@proton.me>
2024-02-20 19:54:12 +0100

    use upcoming notifications

--- composeApp/src/androidMain/kotlin/com/trm/alarmist/core/system/AndroidAlarmScheduler.kt
+++ composeApp/src/androidMain/kotlin/com/trm/alarmist/core/system/AndroidAlarmScheduler.kt
@@ -3,30 +3,49 @@ package com.trm.alarmist.core.system
 import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Context
+import kotlinx.datetime.DateTimeUnit
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.TimeZone
+import kotlinx.datetime.minus
 import kotlinx.datetime.toInstant
 
 class AndroidAlarmScheduler(private val context: Context) : AlarmScheduler {
   private val alarmManager = context.getSystemService(AlarmManager::class.java)
 
   override fun scheduleAlarm(id: Long, fireOnDateTime: LocalDateTime) {
+    alarmManager.setExact(
+      AlarmManager.RTC,
+      fireOnDateTime
+        .toInstant(TimeZone.currentSystemDefault())
+        .minus(1, DateTimeUnit.HOUR) // TODO: this should be in alarm settings
+        .toEpochMilliseconds(),
+      alarmUpcomingPendingIntent(id),
+    )
+
     alarmManager.setExactAndAllowWhileIdle(
       AlarmManager.RTC_WAKEUP,
       fireOnDateTime.toInstant(TimeZone.currentSystemDefault()).toEpochMilliseconds(),
-      alarmPendingIntent(id),
+      alarmFiredPendingIntent(id),
     )
   }
 
   override fun cancelAlarm(id: Long) {
-    alarmManager.cancel(alarmPendingIntent(id))
+    alarmManager.cancel(alarmFiredPendingIntent(id))
   }
 
-  private fun alarmPendingIntent(id: Long): PendingIntent =
+  private fun alarmFiredPendingIntent(id: Long): PendingIntent =
     PendingIntent.getBroadcast(
       context,
       id.toInt(),
       AlarmBroadcastReceiver.alarmFiredIntent(context, id),
+      PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE,
+    )
+
+  private fun alarmUpcomingPendingIntent(id: Long): PendingIntent =
+    PendingIntent.getBroadcast(
+      context,
+      id.toInt(),
+      AlarmBroadcastReceiver.alarmUpcomingIntent(context, id),
       PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE,
     )
 }
