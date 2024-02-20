34c1dee018ca5b4c8dde5b19453eb62f1d6e5583
Tomasz Osoba <tomasz.osoba@knowit.eu>
2024-02-20 08:38:00 +0100

    cleanup android notifications

--- composeApp/src/androidMain/kotlin/com/trm/alarmist/core/system/AndroidAlarmNotifications.kt
+++ composeApp/src/androidMain/kotlin/com/trm/alarmist/core/system/AndroidAlarmNotifications.kt
@@ -4,32 +4,30 @@ import android.app.Application
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
-import android.os.Build
 import androidx.core.app.NotificationCompat
 import com.trm.alarmist.R
 
 fun Context.notifyAlarmFired() {
-  val notificationManager = getSystemService(NotificationManager::class.java)
-  notificationManager.notify(
-    1,
-    NotificationCompat.Builder(this, ALARM_NOTIFICATION_CHANNEL_ID)
-      .setSmallIcon(R.drawable.ic_launcher_foreground)
-      .setContentTitle("Alarm was fired")
-      .setPriority(NotificationCompat.PRIORITY_HIGH)
-      .build(),
-  )
+  getSystemService(NotificationManager::class.java)
+    .notify(
+      1,
+      NotificationCompat.Builder(this, ALARM_NOTIFICATION_CHANNEL_ID)
+        .setSmallIcon(R.drawable.ic_launcher_foreground)
+        .setContentTitle("Alarm was fired")
+        .setPriority(NotificationCompat.PRIORITY_HIGH)
+        .build(),
+    )
 }
 
 fun Application.createAlarmNotificationChannel() {
-  if (Build.VERSION.SDK_INT < Build.VERSION_CODES.O) return
-  val notificationManager = getSystemService(NotificationManager::class.java)
-  notificationManager.createNotificationChannel(
-    NotificationChannel(
-      ALARM_NOTIFICATION_CHANNEL_ID,
-      ALARM_NOTIFICATION_CHANNEL_NAME,
-      NotificationManager.IMPORTANCE_HIGH,
+  getSystemService(NotificationManager::class.java)
+    .createNotificationChannel(
+      NotificationChannel(
+        ALARM_NOTIFICATION_CHANNEL_ID,
+        ALARM_NOTIFICATION_CHANNEL_NAME,
+        NotificationManager.IMPORTANCE_HIGH,
+      )
     )
-  )
 }
 
 internal const val ALARM_NOTIFICATION_CHANNEL_ID = "ALARM_CHANNEL"
