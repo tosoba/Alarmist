b50b803a999ed8d571b40c9a7c83892c7524cb4f
tosoba <therealmerengue@gmail.com>
2024-05-03 12:36:42 +0200

    upcoming/missed alarm notification content title/text with fireOnDateTime

--- composeApp/src/androidMain/kotlin/com/trm/alarmist/core/system/AndroidAlarmNotifications.kt
+++ composeApp/src/androidMain/kotlin/com/trm/alarmist/core/system/AndroidAlarmNotifications.kt
@@ -2,27 +2,36 @@ package com.trm.alarmist.core.system
 
 import alarmist.composeapp.generated.resources.Res
 import alarmist.composeapp.generated.resources.dismiss
+import alarmist.composeapp.generated.resources.missed_alarm
+import alarmist.composeapp.generated.resources.upcoming_alarm
 import android.app.Application
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
+import android.text.format.DateFormat
 import androidx.core.app.NotificationCompat
 import com.trm.alarmist.R
 import com.trm.alarmist.core.common.util.getStringBlocking
 import com.trm.alarmist.core.system.receiver.AlarmDismissedBroadcastReceiver
+import java.time.format.TextStyle
+import java.util.Locale
+import kotlinx.datetime.LocalDateTime
+import kotlinx.datetime.LocalTime
+import kotlinx.datetime.format
+import kotlinx.datetime.format.Padding
+import kotlinx.datetime.format.char
 import org.jetbrains.compose.resources.ExperimentalResourceApi
 
 @OptIn(ExperimentalResourceApi::class)
-fun Context.notifyAlarmUpcoming(
-  settings: AlarmFireSettings
-) { // TODO: show formatted fireOnDateTime in notification
+fun Context.notifyAlarmUpcoming(settings: AlarmFireSettings) {
   getSystemService(NotificationManager::class.java)
     .notify(
       settings.id.toInt(),
       NotificationCompat.Builder(this, ALARM_UPCOMING_NOTIFICATION_CHANNEL_ID)
         .setSmallIcon(R.drawable.ic_launcher_foreground)
-        .setContentTitle("Alarm is upcoming")
+        .setContentTitle(getStringBlocking(Res.string.upcoming_alarm))
+        .setContentText(settings.fireOnDateTime.formattedContentText(this))
         .setSilent(true)
         .addAction(
           R.drawable.ic_launcher_foreground,
@@ -38,19 +47,33 @@ fun Context.notifyAlarmUpcoming(
     )
 }
 
-fun Context.notifyAlarmMissed(
-  settings: AlarmFireSettings
-) { // TODO: show formatted fireOnDateTime in notification + name + group name
+@OptIn(ExperimentalResourceApi::class)
+fun Context.notifyAlarmMissed(settings: AlarmFireSettings) {
   getSystemService(NotificationManager::class.java)
     .notify(
       settings.id.toInt(),
       NotificationCompat.Builder(this, ALARM_MISSED_NOTIFICATION_CHANNEL_ID)
         .setSmallIcon(R.drawable.ic_launcher_foreground)
-        .setContentTitle("Alarm was missed")
+        .setContentTitle(getStringBlocking(Res.string.missed_alarm))
+        .setContentText(settings.fireOnDateTime.formattedContentText(this))
         .setSilent(true)
         .build(),
     )
 }
+
+private fun LocalDateTime.formattedContentText(context: Context): String =
+  "${dayOfWeek.getDisplayName(TextStyle.SHORT, Locale.getDefault())} ${time.format(
+    LocalTime.Format {
+      if (!DateFormat.is24HourFormat(context)) amPmHour(padding = Padding.ZERO) else hour(padding = Padding.ZERO)
+      char(':')
+      minute(padding = Padding.ZERO)
+      if (!DateFormat.is24HourFormat(context)) {
+        char(' ')
+        amPmMarker("AM", "PM")
+      }
+    }
+  )
+  }"
 
 fun Context.cancelNotification(id: Int) {
   getSystemService(NotificationManager::class.java).cancel(id)
