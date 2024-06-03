ec47abd889d25a029b4ce8602a569b4c4e0a25a2
tosoba <therealmerengue@gmail.com>
2024-06-03 19:17:20 +0200

    use factoryOf from koin

--- composeApp/src/commonMain/kotlin/com/trm/alarmist/core/domain/di/DomainModule.kt
+++ composeApp/src/commonMain/kotlin/com/trm/alarmist/core/domain/di/DomainModule.kt
@@ -5,11 +5,11 @@ import com.trm.alarmist.core.domain.usecase.DeleteAlarmUseCase
 import com.trm.alarmist.core.domain.usecase.EditAlarmUseCase
 import com.trm.alarmist.core.domain.usecase.GetAlarmsInGroupFlowUseCase
 import com.trm.alarmist.core.domain.usecase.GetAlarmsScheduledOnDateFlowUseCase
-import com.trm.alarmist.core.domain.usecase.GetTodayAlarmsUseCase
 import com.trm.alarmist.core.domain.usecase.GetAndResetMissedAlarmsOnBootUseCase
 import com.trm.alarmist.core.domain.usecase.GetGroupedAlarmsFlowUseCase
 import com.trm.alarmist.core.domain.usecase.GetNextAlarmUseCase
 import com.trm.alarmist.core.domain.usecase.GetScheduledAlarmCountsForDateRangeUseCase
+import com.trm.alarmist.core.domain.usecase.GetTodayAlarmsUseCase
 import com.trm.alarmist.core.domain.usecase.IsAlarmScheduledToFireAtDateTime
 import com.trm.alarmist.core.domain.usecase.ToggleAlarmOnOffUseCase
 import com.trm.alarmist.core.domain.usecase.TurnAlarmOffOnDateUseCase
@@ -18,25 +18,26 @@ import com.trm.alarmist.core.domain.usecase.UpdateAlarmOnDismissUseCase
 import com.trm.alarmist.core.domain.usecase.UpdateAlarmOnSnoozeUseCase
 import com.trm.alarmist.core.domain.usecase.UpdateAlarmScheduleUseCase
 import com.trm.alarmist.core.domain.usecase.UpdateGroupOnOffUseCase
+import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
 
 val domainModule = module {
-  factory { AddAlarmUseCase(get(), get()) }
-  factory { EditAlarmUseCase(get(), get()) }
-  factory { DeleteAlarmUseCase(get(), get()) }
-  factory { ToggleAlarmOnOffUseCase(get(), get()) }
-  factory { UpdateGroupOnOffUseCase(get(), get()) }
-  factory { UpdateAlarmScheduleUseCase(get()) }
-  factory { UpdateAlarmOnDismissUseCase(get(), get()) }
-  factory { UpdateAlarmOnSnoozeUseCase(get(), get()) }
-  factory { GetGroupedAlarmsFlowUseCase(get()) }
-  factory { GetAlarmsInGroupFlowUseCase(get()) }
-  factory { GetAndResetMissedAlarmsOnBootUseCase(get()) }
-  factory { GetAlarmsScheduledOnDateFlowUseCase(get()) }
-  factory { GetTodayAlarmsUseCase(get()) }
-  factory { GetNextAlarmUseCase(get()) }
-  factory { GetScheduledAlarmCountsForDateRangeUseCase(get()) }
-  factory { IsAlarmScheduledToFireAtDateTime(get()) }
-  factory { TurnAlarmOffOnDateUseCase(get(), get()) }
-  factory { TurnAlarmOnOnDateUseCase(get(), get()) }
+  factoryOf(::AddAlarmUseCase)
+  factoryOf(::EditAlarmUseCase)
+  factoryOf(::DeleteAlarmUseCase)
+  factoryOf(::ToggleAlarmOnOffUseCase)
+  factoryOf(::UpdateGroupOnOffUseCase)
+  factoryOf(::UpdateAlarmScheduleUseCase)
+  factoryOf(::UpdateAlarmOnDismissUseCase)
+  factoryOf(::UpdateAlarmOnSnoozeUseCase)
+  factoryOf(::GetGroupedAlarmsFlowUseCase)
+  factoryOf(::GetAlarmsInGroupFlowUseCase)
+  factoryOf(::GetAndResetMissedAlarmsOnBootUseCase)
+  factoryOf(::GetAlarmsScheduledOnDateFlowUseCase)
+  factoryOf(::GetTodayAlarmsUseCase)
+  factoryOf(::GetNextAlarmUseCase)
+  factoryOf(::GetScheduledAlarmCountsForDateRangeUseCase)
+  factoryOf(::IsAlarmScheduledToFireAtDateTime)
+  factoryOf(::TurnAlarmOffOnDateUseCase)
+  factoryOf(::TurnAlarmOnOnDateUseCase)
 }
--- composeApp/src/iosMain/kotlin/com/trm/alarmist/core/system/di/iOSSystemModule.kt
+++ composeApp/src/iosMain/kotlin/com/trm/alarmist/core/system/di/iOSSystemModule.kt
@@ -2,6 +2,7 @@ package com.trm.alarmist.core.system.di
 
 import com.trm.alarmist.core.system.AlarmScheduler
 import org.koin.core.module.Module
+import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.module
 
-actual val systemModule: Module = module { factory<AlarmScheduler> { IosAlarmScheduler() } }
+actual val systemModule: Module = module { factoryOf<AlarmScheduler>(::IosAlarmScheduler) }
