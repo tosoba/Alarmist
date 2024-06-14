d5103ffb48701390b0a08bfcdfca2f485dd5400a
Tomasz Osoba <tomasz.osoba@knowit.eu>
2024-06-14 16:05:44 +0200

    use bold typeface + font size in XML textView in widgets

--- composeApp/src/androidMain/kotlin/com/trm/alarmist/widget/common/ui/WidgetAlarmFireAtTimeText.kt
+++ composeApp/src/androidMain/kotlin/com/trm/alarmist/widget/common/ui/WidgetAlarmFireAtTimeText.kt
@@ -1,14 +1,18 @@
 package com.trm.alarmist.widget.common.ui
 
+import android.graphics.Typeface
+import android.text.Spannable
 import android.text.SpannableString
 import android.text.Spanned
 import android.text.style.RelativeSizeSpan
+import android.text.style.StyleSpan
 import android.widget.RemoteViews
 import androidx.compose.runtime.Composable
 import androidx.glance.GlanceModifier
 import androidx.glance.LocalContext
 import androidx.glance.appwidget.AndroidRemoteViews
 import androidx.glance.layout.Box
+import androidx.glance.text.FontWeight
 import androidx.glance.text.TextDefaults
 import androidx.glance.text.TextStyle
 import com.trm.alarmist.R
@@ -36,16 +40,19 @@ fun WidgetAlarmFireAtTimeText(
                 fireAtTime.toFormattedString { is24HourFormat }
               }
               .replace(" ", "\u200A")
+
           setTextViewText(
             R.id.widget_text_view,
             SpannableString(fireAtTimeText).apply {
               val amPmIndex = fireAtTimeText.indexOfAny(listOf("am", "pm"), ignoreCase = true)
+
               setSpan(
                 RelativeSizeSpan(1f),
                 0,
                 if (amPmIndex != -1) amPmIndex else fireAtTimeText.length,
                 Spanned.SPAN_EXCLUSIVE_EXCLUSIVE,
               )
+
               if (amPmIndex != -1) {
                 setSpan(
                   RelativeSizeSpan(.5f),
@@ -54,23 +61,20 @@ fun WidgetAlarmFireAtTimeText(
                   Spanned.SPAN_EXCLUSIVE_EXCLUSIVE,
                 )
               }
+
+              if (style.fontWeight?.value != FontWeight.Normal.value) {
+                setSpan(
+                  StyleSpan(Typeface.BOLD),
+                  0,
+                  fireAtTimeText.length,
+                  Spannable.SPAN_INCLUSIVE_INCLUSIVE,
+                )
+              }
             },
           )
+
+          style.fontSize?.value?.let { setFloat(R.id.widget_text_view, "setTextSize", it) }
         }
     )
   }
-
-  //  Text(
-  //    text =
-  //      if (useFullFormat) {
-  //        """${fireAtTime.toFormattedString { is24HourFormat }} ${fireAtTime.amPmString {
-  // is24HourFormat }}"""
-  //          .trim()
-  //      } else {
-  //        fireAtTime.toFormattedString { is24HourFormat }
-  //      },
-  //    modifier = modifier,
-  //    style = style,
-  //    maxLines = 1,
-  //  )
 }
