ff6e1cfa8c3b9a988f0ed0e9d397ba1c36ce30f7
Mere Ngue <therealmerengue@gmail.com>
2024-06-15 12:16:27 +0200

    all possible version upgrades

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
   alias(libs.plugins.kotlinMultiplatform)
   alias(libs.plugins.androidApplication)
   alias(libs.plugins.jetbrainsCompose)
+  alias(libs.plugins.compose.compiler)
   alias(libs.plugins.kotlinx.serialization)
   alias(libs.plugins.kotlinx.parcelize)
   alias(libs.plugins.sqlDelight)
