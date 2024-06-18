c2315d05b9079a262a524e3aa90c5cde5ebdb445
Mere Ngue <therealmerengue@gmail.com>
2024-06-18 20:21:28 +0200

    add more CalculateScheduledAlarmNextFireOnDateForDaysOfWeekTests

--- composeApp/src/commonTest/kotlin/com/trm/alarmist/core/domain/CalculateScheduledAlarmNextFireOnDateForDaysOfWeekTests.kt
+++ composeApp/src/commonTest/kotlin/com/trm/alarmist/core/domain/CalculateScheduledAlarmNextFireOnDateForDaysOfWeekTests.kt
@@ -3,10 +3,15 @@ package com.trm.alarmist.core.domain
 import com.trm.alarmist.core.domain.usecase.calculateScheduledAlarmNextFireOnDateForDaysOfWeek
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertNotNull
+import kotlin.test.assertTrue
+import kotlinx.datetime.DateTimeUnit
+import kotlinx.datetime.DayOfWeek
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.LocalTime
 import kotlinx.datetime.Month
+import kotlinx.datetime.plus
 
 class CalculateScheduledAlarmNextFireOnDateForDaysOfWeekTests {
   @Test
@@ -26,5 +31,49 @@ class CalculateScheduledAlarmNextFireOnDateForDaysOfWeekTests {
         afterDateTime = afterDateTime,
       ),
     )
+  }
+
+  @Test
+  fun `given day of week equal to afterDateTime day of week and fireAtTime before afterDateTime and empty offOnDates - then return date 1 week after afterDateTime`() {
+    val afterDateTime =
+      LocalDateTime(
+        LocalDate(year = 2024, month = Month.JULY, dayOfMonth = 20),
+        LocalTime(hour = 11, minute = 55, second = 1),
+      )
+
+    assertEquals(
+      afterDateTime.date.plus(7L, DateTimeUnit.DAY),
+      calculateScheduledAlarmNextFireOnDateForDaysOfWeek(
+        scheduledOnDaysOfWeek = listOf(afterDateTime.dayOfWeek),
+        fireAtTime = LocalTime(hour = 11, minute = 55),
+        offOnDates = emptyList(),
+        afterDateTime = afterDateTime,
+      ),
+    )
+  }
+
+  @Test
+  fun `given day of week not equal to afterDateTime day of week and empty offOnDates - then return next appropriate day of week after afterDateTime`() {
+    val afterDateTime =
+      LocalDateTime(
+        LocalDate(year = 2024, month = Month.JULY, dayOfMonth = 20),
+        LocalTime(hour = 11, minute = 55, second = 1),
+      )
+
+    DayOfWeek.entries
+      .filter { it != afterDateTime.dayOfWeek }
+      .forEach {
+        val result =
+          calculateScheduledAlarmNextFireOnDateForDaysOfWeek(
+            scheduledOnDaysOfWeek = listOf(it),
+            fireAtTime = LocalTime(hour = 16, minute = 55),
+            offOnDates = emptyList(),
+            afterDateTime = afterDateTime,
+          )
+
+        assertNotNull(result)
+        assertEquals(it, result.dayOfWeek)
+        assertTrue(result > afterDateTime.date)
+      }
   }
 }
