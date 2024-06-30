e311a54add122094b17f92a6b06bb79d648c04c5
Mere Ngue <therealmerengue@gmail.com>
2024-06-30 12:28:28 +0200

    use widget pinned string res

--- composeApp/src/androidMain/kotlin/com/trm/alarmist/widget/common/WidgetPinnedReceiver.kt
+++ composeApp/src/androidMain/kotlin/com/trm/alarmist/widget/common/WidgetPinnedReceiver.kt
@@ -5,10 +5,15 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.widget.Toast
+import com.trm.alarmist.R
 
 class WidgetPinnedReceiver : BroadcastReceiver() {
   override fun onReceive(context: Context, intent: Intent) {
-    Toast.makeText(context, "Widget pinned. Check it on your homescreen.", Toast.LENGTH_SHORT)
+    Toast.makeText(
+        context,
+        context.getString(R.string.widget_pinned_success_info),
+        Toast.LENGTH_SHORT,
+      )
       .show()
   }
 
