bc8c85f316173253be38499f9ca9b9db0dd186b9
Mere Ngue <therealmerengue@gmail.com>
2024-06-30 13:47:42 +0200

    app widget configure works from both widget list pin and home screen launcher

--- composeApp/src/androidMain/kotlin/com/trm/alarmist/widget/group/AlarmGroupWidgetConfigActivity.kt
+++ composeApp/src/androidMain/kotlin/com/trm/alarmist/widget/group/AlarmGroupWidgetConfigActivity.kt
@@ -1,24 +1,49 @@
 package com.trm.alarmist.widget.group
 
 import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 
 class AlarmGroupWidgetConfigActivity : ComponentActivity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    // TODO: when setting widget config refer always to the widget that was most recently pinned
+
+    setResult(RESULT_CANCELED)
+
+    val appWidgetId =
+      intent.getIntExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID)
+    if (appWidgetId == AppWidgetManager.INVALID_APPWIDGET_ID) finish()
+
     setContent {
-      Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+      Column(modifier = Modifier.fillMaxSize()) {
         Text(text = "Alarm group config activity")
+
+        Row {
+          Button(onClick = { finish() }) { Text(text = "Cancel") }
+          Button(
+            onClick = {
+              // TODO: update widget like in daylighter
+
+              setResult(
+                RESULT_OK,
+                Intent().putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetId),
+              )
+              finish()
+            }
+          ) {
+            Text(text = "Ok")
+          }
+        }
       }
     }
   }
@@ -29,7 +54,8 @@ class AlarmGroupWidgetConfigActivity : ComponentActivity() {
         context,
         0,
         Intent(context, AlarmGroupWidgetConfigActivity::class.java),
-        PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE,
+        // must have FLAG_MUTABLE - otherwise EXTRA_APPWIDGET_ID will not be set
+        PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE,
       )
   }
 }
