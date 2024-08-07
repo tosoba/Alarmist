e15decc4056d98571daaf5f26180b77c08801337
Tomasz Osoba <tomasz.osoba@knowit.eu>
2024-08-07 15:49:22 +0200

    Squashed commit of the following:
    commit 3085f5e856cfe6a3707a4ddd165eee4f6c85f86f
    Author: Tomasz Osoba <tomasz.osoba@knowit.eu>
    Date:   Wed Aug 7 15:49:07 2024 +0200
    
        cleanup alarms group list
    
    commit d3476edff040d1a71e3c01c15ae41fccb929d393
    Author: Tomasz Osoba <tomasz.osoba@knowit.eu>
    Date:   Wed Aug 7 15:45:30 2024 +0200
    
        working corners logic in alarms group
    
    commit 81a6fc7046941d66015eac6eaf191b2220cfcc63
    Author: Mere Ngue <therealmerengue@gmail.com>
    Date:   Tue Aug 6 20:37:49 2024 +0200
    
        add divider to edit group button + not working alarm index shape logic
    
    commit c1a0ee57090769108591be4de088a7b14ad3b0ee
    Author: Mere Ngue <therealmerengue@gmail.com>
    Date:   Mon Aug 5 21:29:14 2024 +0200
    
        ultra basic alarm groups grid setup

--- composeApp/src/commonMain/kotlin/com/trm/alarmist/core/ui/AlarmGroupsList.kt
+++ composeApp/src/commonMain/kotlin/com/trm/alarmist/core/ui/AlarmGroupsList.kt
@@ -6,14 +6,17 @@ import alarmist.composeapp.generated.resources.edit_group
 import alarmist.composeapp.generated.resources.no_alarm_groups_created
 import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.LazyListScope
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.GridItemSpan
+import androidx.compose.foundation.lazy.grid.LazyGridScope
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.CreateNewFolder
@@ -28,7 +31,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
 import com.trm.alarmist.core.common.util.elevatedIf
 import com.trm.alarmist.core.domain.model.AlarmGroupModel
@@ -46,7 +48,7 @@ fun AlarmGroupsList(
   onEditGroupClick: (AlarmGroupModel) -> Unit = {},
   onAlarmItemClick: (AlarmListModel) -> Unit = {},
   onToggleAlarmOnOff: (AlarmListModel) -> Unit = {},
-  headerItems: LazyListScope.() -> Unit = {},
+  headerItems: LazyGridScope.() -> Unit = {},
   groupHeaderCardTrailing: @Composable (AlarmGroupModel) -> Unit,
 ) {
   Crossfade(groups.isEmpty(), modifier = modifier) { groupsEmpty ->
@@ -67,89 +69,150 @@ fun AlarmGroupsList(
         FloatingActionButtonSpacer()
       }
     } else {
-      LazyColumn(
-        modifier = Modifier.fillMaxSize(),
-        contentPadding = PaddingValues(horizontal = 16.dp, vertical = 8.dp),
-      ) {
-        headerItems()
+      BoxWithConstraints(modifier = Modifier.fillMaxSize()) {
+        val cellMinSize = 300.dp
+        val contentPaddingHorizontal = 16.dp
 
-        groups.forEachIndexed { groupIndex, group ->
-          val isExpanded = expandedGroupId == group.id
+        LazyVerticalGrid(
+          modifier = Modifier.fillMaxSize(),
+          columns = GridCells.Adaptive(minSize = cellMinSize),
+          contentPadding = PaddingValues(horizontal = contentPaddingHorizontal, vertical = 8.dp),
+        ) {
+          headerItems()
 
-          item(key = "group-${group.id}") {
-            val shape =
-              if (isExpanded) {
-                ShapeDefaults.Medium.copy(
-                  bottomStart = CornerSize(0.dp),
-                  bottomEnd = CornerSize(0.dp),
-                )
-              } else {
-                ShapeDefaults.Medium
-              }
-            AlarmGroupHeaderCard(
-              group = group,
-              onClick = {
-                if (group.alarmsCount > 0L) {
-                  if (isExpanded) onCollapseGroup() else onExpandGroup(group)
+          groups.forEachIndexed { groupIndex, group ->
+            val isExpanded = expandedGroupId == group.id
+
+            item(key = "group-${group.id}", span = { GridItemSpan(maxLineSpan) }) {
+              val shape =
+                if (isExpanded) {
+                  ShapeDefaults.Medium.copy(
+                    bottomStart = CornerSize(0.dp),
+                    bottomEnd = CornerSize(0.dp),
+                  )
                 } else {
-                  onEditGroupClick(group)
+                  ShapeDefaults.Medium
                 }
-              },
-              modifier = Modifier.fillMaxWidth().padding(top = if (groupIndex > 0) 16.dp else 0.dp),
-              shape = shape,
-              trailing = { groupHeaderCardTrailing(group) },
-            )
-          }
+              AlarmGroupHeaderCard(
+                group = group,
+                onClick = {
+                  if (group.alarmsCount > 0L) {
+                    if (isExpanded) onCollapseGroup() else onExpandGroup(group)
+                  } else {
+                    onEditGroupClick(group)
+                  }
+                },
+                modifier =
+                  Modifier.fillMaxWidth().padding(top = if (groupIndex > 0) 16.dp else 0.dp),
+                shape = shape,
+                trailing = { groupHeaderCardTrailing(group) },
+              )
+            }
 
-          if (isExpanded && expandedGroupAlarms.isNotEmpty()) {
-            items(expandedGroupAlarms, key = { alarm -> "alarm-${alarm.id}" }) { alarm ->
-              Box(modifier = Modifier.fillMaxWidth()) {
-                AlarmListItem(
-                  item = alarm,
-                  modifier = Modifier.fillMaxWidth(),
-                  shape = RectangleShape,
-                  onItemClick = onAlarmItemClick,
-                  onToggleOnOff = remember(alarm) { { onToggleAlarmOnOff(alarm) } },
-                )
+            val fullSpan =
+              maxOf(
+                ((maxWidth - contentPaddingHorizontal - contentPaddingHorizontal) / cellMinSize)
+                  .toInt(),
+                1,
+              )
+            if (isExpanded && expandedGroupAlarms.isNotEmpty()) {
+              val firstInLastRowAlarmIndex =
+                expandedGroupAlarms.indices.lastOrNull { it % fullSpan == 0 }
+              val lastInLastRowAlarmIndex =
+                expandedGroupAlarms.indices.lastOrNull { it % fullSpan == fullSpan - 1 }
 
-                HorizontalDivider(
-                  modifier =
-                    Modifier.fillMaxWidth().padding(horizontal = 16.dp).align(Alignment.TopCenter)
-                )
+              itemsIndexed(
+                items = expandedGroupAlarms,
+                key = { _, alarm -> "alarm-${alarm.id}" },
+              ) { index, alarm ->
+                Box(modifier = Modifier.fillMaxWidth()) {
+                  AlarmListItem(
+                    item = alarm,
+                    modifier = Modifier.fillMaxWidth(),
+                    shape =
+                      ShapeDefaults.Medium.copy(
+                        topStart = CornerSize(0.dp),
+                        topEnd = CornerSize(0.dp),
+                        bottomStart =
+                          if (
+                            index == firstInLastRowAlarmIndex &&
+                              group.alarmsCount.toInt() % fullSpan != 0
+                          ) {
+                            ShapeDefaults.Medium.bottomStart
+                          } else {
+                            CornerSize(0.dp)
+                          },
+                        bottomEnd =
+                          if (
+                            (index == lastInLastRowAlarmIndex ||
+                              index == expandedGroupAlarms.lastIndex) && fullSpan != 1
+                          ) {
+                            ShapeDefaults.Medium.bottomEnd
+                          } else {
+                            CornerSize(0.dp)
+                          },
+                      ),
+                    onItemClick = onAlarmItemClick,
+                    onToggleOnOff = remember(alarm) { { onToggleAlarmOnOff(alarm) } },
+                  )
+
+                  HorizontalDivider(
+                    modifier =
+                      Modifier.fillMaxWidth().padding(horizontal = 16.dp).align(Alignment.TopCenter)
+                  )
+                }
               }
             }
-          }
 
-          if (isExpanded && group.alarmsCount > 0L) {
-            item {
-              Card(
-                modifier = Modifier.fillMaxWidth(),
-                colors =
-                  if (group.isOn) {
-                    CardDefaults.cardColors(
-                      containerColor = MaterialTheme.colorScheme.secondaryContainer
+            if (isExpanded && group.alarmsCount > 0L) {
+              item {
+                Card(
+                  modifier = Modifier.fillMaxWidth(),
+                  colors =
+                    if (group.isOn) {
+                      CardDefaults.cardColors(
+                        containerColor = MaterialTheme.colorScheme.secondaryContainer
+                      )
+                    } else {
+                      CardDefaults.cardColors()
+                    },
+                  shape =
+                    if (group.alarmsCount.toInt() % fullSpan == 0) {
+                      ShapeDefaults.Medium.copy(
+                        topStart = CornerSize(0.dp),
+                        topEnd = CornerSize(0.dp),
+                      )
+                    } else {
+                      ShapeDefaults.Medium.copy(
+                        topStart = CornerSize(0.dp),
+                        topEnd = CornerSize(0.dp),
+                        bottomStart = CornerSize(0.dp),
+                      )
+                    },
+                  elevation = CardDefaults.elevatedIf(group.isOn),
+                ) {
+                  Box(modifier = Modifier.fillMaxWidth()) {
+                    HorizontalDivider(
+                      modifier =
+                        Modifier.fillMaxWidth()
+                          .padding(horizontal = 16.dp)
+                          .align(Alignment.TopCenter)
                     )
-                  } else {
-                    CardDefaults.cardColors()
-                  },
-                shape =
-                  ShapeDefaults.Medium.copy(topStart = CornerSize(0.dp), topEnd = CornerSize(0.dp)),
-                elevation = CardDefaults.elevatedIf(group.isOn),
-              ) {
-                Box(Modifier.fillMaxWidth()) {
-                  TextButton(
-                    onClick = remember(group) { { onEditGroupClick(group) } },
-                    modifier = Modifier.fillMaxWidth().align(Alignment.Center),
-                  ) {
-                    Text(stringResource(Res.string.edit_group))
+
+                    TextButton(
+                      onClick = remember(group) { { onEditGroupClick(group) } },
+                      modifier = Modifier.fillMaxWidth().align(Alignment.Center),
+                    ) {
+                      Text(stringResource(Res.string.edit_group))
+                    }
                   }
                 }
               }
             }
           }
-        }
 
-        floatingActionButtonSpacerItem()
+          floatingActionButtonSpacerItem()
+        }
       }
     }
   }
