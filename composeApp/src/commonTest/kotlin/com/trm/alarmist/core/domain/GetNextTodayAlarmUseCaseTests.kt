451e4f1370734d77036d75c1f3e9729eb8aad545
Tomasz Osoba <tomasz.osoba@knowit.eu>
2024-09-30 10:19:56 +0200

    complete GetNextTodayAlarmUseCaseTests

--- composeApp/src/commonTest/kotlin/com/trm/alarmist/core/domain/GetNextTodayAlarmUseCaseTests.kt
+++ composeApp/src/commonTest/kotlin/com/trm/alarmist/core/domain/GetNextTodayAlarmUseCaseTests.kt
@@ -11,9 +11,11 @@ import kotlin.test.Test
 import kotlin.test.assertEquals
 import kotlin.test.assertNull
 import kotlinx.coroutines.test.runTest
+import kotlinx.datetime.DateTimeUnit
 import kotlinx.datetime.LocalDate
 import kotlinx.datetime.LocalDateTime
 import kotlinx.datetime.LocalTime
+import kotlinx.datetime.plus
 
 class GetNextTodayAlarmUseCaseTests {
   @Test
@@ -24,25 +26,80 @@ class GetNextTodayAlarmUseCaseTests {
   }
 
   @Test
-  fun `given multiple on alarms - then return next on alarm scheduled for today`() {
+  fun `given multiple on alarms scheduled for today - then return next on alarm`() = runTest {
     val expectedFireAtTime = LocalDateTime(LocalDate.now(), LocalTime(11, 45))
+    assertEquals(
+      expected = expectedFireAtTime,
+      actual =
+        GetNextTodayAlarmUseCase(
+            alarmRepository(
+              listOf(
+                alarmModel(fireAtTime = LocalTime(12, 25), isOn = true),
+                alarmModel(fireAtTime = LocalTime(12, 35), isOn = true),
+                alarmModel(fireAtTime = LocalTime(15, 15), isOn = true),
+                alarmModel(fireAtTime = expectedFireAtTime.time, isOn = true),
+              )
+            )
+          )(now = LocalDateTime(expectedFireAtTime.date, LocalTime(11, 15, 45)))
+          ?.fireOnDateTime,
+    )
+  }
+
+  @Test
+  fun `given multiple on alarms scheduled for different days - then return next on alarm scheduled for today`() =
     runTest {
+      val expectedFireAtTime = LocalDateTime(LocalDate.now(), LocalTime(11, 45))
       assertEquals(
         expected = expectedFireAtTime,
         actual =
           GetNextTodayAlarmUseCase(
               alarmRepository(
                 listOf(
-                  alarmModel(fireAtTime = LocalTime(12, 25), isOn = true),
                   alarmModel(fireAtTime = LocalTime(12, 35), isOn = true),
-                  alarmModel(fireAtTime = LocalTime(15, 15), isOn = true),
                   alarmModel(fireAtTime = expectedFireAtTime.time, isOn = true),
+                  alarmModel(fireAtTime = LocalTime(10, 15), isOn = true),
+                  alarmModel(
+                    fireAtTime = LocalTime(11, 40),
+                    isOn = true,
+                    scheduledOnDaysOfWeek =
+                      setOf(expectedFireAtTime.date.plus(1L, DateTimeUnit.DAY).dayOfWeek),
+                  ),
+                  alarmModel(
+                    fireAtTime = LocalTime(11, 40),
+                    isOn = true,
+                    scheduledOnDates = setOf(expectedFireAtTime.date.plus(1L, DateTimeUnit.DAY)),
+                  ),
                 )
               )
             )(now = LocalDateTime(expectedFireAtTime.date, LocalTime(11, 15, 45)))
             ?.fireOnDateTime,
       )
     }
+
+  @Test
+  fun `given only alarms with null fireOnDateTime - then return null`() = runTest {
+    val today = LocalDate.now()
+    assertNull(
+      GetNextTodayAlarmUseCase(
+          alarmRepository(
+            listOf(
+              alarmModel(
+                fireAtTime = LocalTime(11, 40),
+                isOn = true,
+                scheduledOnDaysOfWeek = setOf(today.dayOfWeek),
+                offOnDates = setOf(today),
+              ),
+              alarmModel(
+                fireAtTime = LocalTime(11, 40),
+                isOn = true,
+                scheduledOnDates = setOf(today),
+                offOnDates = setOf(today),
+              ),
+            )
+          )
+        )(now = LocalDateTime(today, LocalTime(11, 15, 45)))
+        ?.fireOnDateTime
+    )
   }
 
   private fun alarmRepository(onAlarms: List<AlarmModel>): AlarmRepository = mock {
