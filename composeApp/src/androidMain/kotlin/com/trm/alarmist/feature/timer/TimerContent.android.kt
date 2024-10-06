3d0befeb4032e17b1381051805e348ac8cc4f7c5
Mere Ngue <therealmerengue@gmail.com>
2024-10-06 13:45:42 +0200

    show timer scaffold only when service is not null

--- composeApp/src/androidMain/kotlin/com/trm/alarmist/feature/timer/TimerContent.android.kt
+++ composeApp/src/androidMain/kotlin/com/trm/alarmist/feature/timer/TimerContent.android.kt
@@ -5,17 +5,16 @@ import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
 import android.os.IBinder
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.State
 import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.Saver
-import androidx.compose.runtime.saveable.SaverScope
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -24,7 +23,6 @@ import com.trm.alarmist.core.domain.model.TimerState
 import com.trm.alarmist.core.system.permission.postNotificationsPermissionHandler
 import com.trm.alarmist.core.system.timer.TimerService
 import kotlin.time.Duration
-import kotlin.time.Duration.Companion.milliseconds
 import kotlin.time.Duration.Companion.minutes
 
 @Composable
@@ -57,18 +55,9 @@ actual fun TimerContent(component: TimerComponent, modifier: Modifier) {
     onDispose { if (bound) context.unbindService(connection) }
   }
 
-  val state by
-    rememberSaveable(service, saver = TimerStateSaver) {
-      derivedStateOf { service?.state ?: TimerState.IDLE }
-    }
-  val duration by
-    rememberSaveable(service, saver = DurationSaver) {
-      derivedStateOf { service?.duration ?: Duration.ZERO }
-    }
-  val initialDuration by
-    rememberSaveable(service, saver = DurationSaver) {
-      derivedStateOf { service?.initialDuration ?: Duration.ZERO }
-    }
+  val state by remember { derivedStateOf { service?.state ?: TimerState.IDLE } }
+  val duration by remember { derivedStateOf { service?.duration ?: Duration.ZERO } }
+  val initialDuration by remember { derivedStateOf { service?.initialDuration ?: Duration.ZERO } }
 
   DisposableEffect(component.lifecycle) {
     val callbacks =
@@ -91,50 +80,40 @@ actual fun TimerContent(component: TimerComponent, modifier: Modifier) {
     onDispose { component.lifecycle.unsubscribe(callbacks) }
   }
 
-  TimerScaffold(
-    duration = service?.duration ?: duration,
-    initialDuration = service?.initialDuration ?: initialDuration,
-    state = service?.state ?: state,
-    onStartClick = { newDuration ->
-      if (newDuration.inWholeSeconds > 0L) {
+  AnimatedVisibility(visible = service != null, enter = fadeIn(), exit = fadeOut()) {
+    TimerScaffold(
+      duration = duration,
+      initialDuration = initialDuration,
+      state = state,
+      onStartClick = { newDuration ->
+        if (newDuration.inWholeSeconds > 0L) {
+          TimerService.startWithAction(
+            context = context,
+            action = TimerService.Action.Start(newDuration),
+          )
+        }
+      },
+      onToggleRunningClick = {
+        TimerService.startWithAction(context = context, action = TimerService.Action.ToggleRunning)
+      },
+      onCancelClick = {
+        TimerService.startWithAction(context = context, action = TimerService.Action.Cancel)
+      },
+      onResetClick = {
+        TimerService.startWithAction(context = context, action = TimerService.Action.Reset)
+      },
+      onAddMinuteClick = {
         TimerService.startWithAction(
           context = context,
-          action = TimerService.Action.Start(newDuration),
+          action = TimerService.Action.AddDuration(1.minutes),
         )
-      }
-    },
-    onToggleRunningClick = {
-      TimerService.startWithAction(context = context, action = TimerService.Action.ToggleRunning)
-    },
-    onCancelClick = {
-      TimerService.startWithAction(context = context, action = TimerService.Action.Cancel)
-    },
-    onResetClick = {
-      TimerService.startWithAction(context = context, action = TimerService.Action.Reset)
-    },
-    onAddMinuteClick = {
-      TimerService.startWithAction(
-        context = context,
-        action = TimerService.Action.AddDuration(1.minutes),
-      )
-    },
-    onSubtractMinuteClick = {
-      TimerService.startWithAction(
-        context = context,
-        action = TimerService.Action.SubtractDuration(1.minutes),
-      )
-    },
-  )
-}
-
-private object DurationSaver : Saver<State<Duration>, Long> {
-  override fun SaverScope.save(value: State<Duration>): Long = value.value.inWholeMilliseconds
-
-  override fun restore(value: Long): State<Duration> = mutableStateOf(value.milliseconds)
-}
-
-private object TimerStateSaver : Saver<State<TimerState>, String> {
-  override fun SaverScope.save(value: State<TimerState>): String = value.value.name
-
-  override fun restore(value: String): State<TimerState> = mutableStateOf(TimerState.valueOf(value))
+      },
+      onSubtractMinuteClick = {
+        TimerService.startWithAction(
+          context = context,
+          action = TimerService.Action.SubtractDuration(1.minutes),
+        )
+      },
+    )
+  }
 }
