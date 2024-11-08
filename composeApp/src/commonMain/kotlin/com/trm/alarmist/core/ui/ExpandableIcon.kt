e4d40dc83368b3f3c0865b5de0aa04bb660eda24
merengue <therealmerengue@proton.me>
2024-11-08 20:11:39 +0100

    calendar expandable icon instead of image

--- composeApp/src/commonMain/kotlin/com/trm/alarmist/core/ui/ExpandableIcon.kt
+++ composeApp/src/commonMain/kotlin/com/trm/alarmist/core/ui/ExpandableIcon.kt
@@ -2,9 +2,9 @@ package com.trm.alarmist.core.ui
 
 import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.updateTransition
-import androidx.compose.foundation.Image
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.ExpandMore
+import androidx.compose.material3.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
@@ -16,17 +16,16 @@ fun ExpandableIcon(
   modifier: Modifier = Modifier,
   transitionLabel: String = "ExpandableIcon",
 ) {
-  val expandedTransition =
+  val rotation by
     updateTransition(
-      targetState = isExpanded,
-      label = "updateTransition-$transitionLabel-isExpanded",
-    )
-  val expandImageRotation by
-    expandedTransition.animateFloat(label = "animateFloat-$transitionLabel-rotation") { state ->
-      if (state) 180f else 0f
-    }
-  Image(
-    modifier = modifier.rotate(expandImageRotation),
+        targetState = isExpanded,
+        label = "updateTransition-$transitionLabel-isExpanded",
+      )
+      .animateFloat(label = "animateFloat-$transitionLabel-rotation") { state ->
+        if (state) 180f else 0f
+      }
+  Icon(
+    modifier = modifier.rotate(rotation),
     imageVector = Icons.Outlined.ExpandMore,
     contentDescription = null,
   )
