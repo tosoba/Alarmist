7ff9640f0df3aa96764ee46235f081c3bb76c3ef
tosoba <therealmerengue@gmail.com>
2026-05-09 11:03:08 +0200

    Fix AppModuleTests

--- composeApp/src/androidHostTest/kotlin/com/trm/alarmist/core/AppModuleTests.kt
+++ composeApp/src/androidHostTest/kotlin/com/trm/alarmist/core/AppModuleTests.kt
@@ -1,14 +1,28 @@
 package com.trm.alarmist.core
 
 import com.trm.alarmist.appModule
+import com.trm.alarmist.core.system.AlarmScheduler
+import com.trm.alarmist.core.system.WidgetManager
+import com.trm.alarmist.feature.stopwatch.StopwatchScreenProvider
+import com.trm.alarmist.feature.timer.TimerScreenProvider
+import com.trm.alarmist.feature.widgets.WidgetScreenProvider
+import kotlin.test.Test
 import org.koin.core.annotation.KoinExperimentalAPI
 import org.koin.test.verify.verify
-import kotlin.test.Test
 
 @OptIn(KoinExperimentalAPI::class)
 class AppModuleTests {
   @Test
   fun `appModule should be verified positively`() {
-    appModule.verify()
+    appModule.verify(
+      extraTypes =
+        listOf(
+          WidgetManager::class,
+          AlarmScheduler::class,
+          WidgetScreenProvider::class,
+          StopwatchScreenProvider::class,
+          TimerScreenProvider::class,
+        )
+    )
   }
 }
