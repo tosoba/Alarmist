e6778cd43ba769b22fa2da5a7d8775496dbcec48
tosoba <therealmerengue@gmail.com>
2026-05-29 20:35:19 +0200

    Cleanup ActivityIntentUtil

--- composeApp/src/androidMain/kotlin/com/trm/alarmist/core/common/util/ActivityIntentUtil.kt
+++ composeApp/src/androidMain/kotlin/com/trm/alarmist/core/common/util/ActivityIntentUtil.kt
@@ -4,25 +4,21 @@ import android.appwidget.AppWidgetManager
 import android.content.Context
 import android.content.Intent
 
-fun createMainActivityIntent(context: Context): Intent {
-  return Intent().setClassName(context.packageName, "com.trm.alarmist.MainActivity")
-}
+fun createMainActivityIntent(context: Context): Intent =
+  Intent().setClassName(context.packageName, "com.trm.alarmist.MainActivity")
 
-fun createAlarmFiredActivityIntent(context: Context): Intent {
-  return Intent().setClassName(context.packageName, "com.trm.alarmist.AlarmFiredActivity")
-}
+fun createAlarmFiredActivityIntent(context: Context): Intent =
+  Intent().setClassName(context.packageName, "com.trm.alarmist.AlarmFiredActivity")
 
-fun createGroupWidgetPinIntent(context: Context, widgetId: Int): Intent {
-  return Intent()
+fun createGroupWidgetPinIntent(context: Context, widgetId: Int): Intent =
+  Intent()
     .setClassName(context.packageName, "com.trm.alarmist.widget.group.GroupWidgetConfigActivity")
     .putExtra("IS_PINNED", true)
     .putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetId)
-}
 
-fun createGroupWidgetActionIntent(context: Context, widgetId: Int): Intent {
-  return Intent()
+fun createGroupWidgetActionIntent(context: Context, widgetId: Int): Intent =
+  Intent()
     .setClassName(context.packageName, "com.trm.alarmist.widget.group.GroupWidgetConfigActivity")
     .putExtra("IS_PINNED", true)
     .putExtra("WIDGET_ACTION", true)
     .putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetId)
-}
